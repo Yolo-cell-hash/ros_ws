9216748bf42e0778458b39c6a23a3e9fb70c38cc
Jay Keer <85948257+Yolo-cell-hash@users.noreply.github.com>
2023-12-08 15:18:25 +0530

    Add files via upload

---
+++ min_code/src/publisher.cpp
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+int main(int argc, char **argv){
+  ros::init(argc,argv,"min_publisher");
+  ros::NodeHandle n;
+  ros::Publisher my_publisher_obj=n.advertise<std_msgs::Float64>("topic1",1);
+  std_msgs::Float64 input_float;
+  
+  ros::Rate naptime(1.0);
+  
+  input_float.data=0.0;
+  
+  while(ros::ok()){
+  input_float.data+=1;
+  my_publisher_obj.publish(input_float);
+  
+  naptime.sleep();
+ }
+
+
+
+}
---
+++ min_code/src/subscriber.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+void myCallback(const std_msgs::Float64& message_holder){
+  ROS_INFO("Recieved value is: %f",message_holder.data);
+}
+
+int main(int argc, char **argv){
+  ros::init(argc,argv,"min_subscriber");
+  ros::NodeHandle n;
+  
+  ros::Subscriber my_subscriber_obj=n.subscribe("topic1",1,myCallback);
+  ros::spin();
+  
+  return 0;
+}
+
